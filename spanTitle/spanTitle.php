247c18def4f4173cd99dd3fabe5a15b67527a6d9
Grib Cyr <1832331+gcyrillus@users.noreply.github.com>
2022-04-26 17:59:03 +0200

    Update spanTitle.php
    [+] Deactivate GZIP

--- spanTitle/spanTitle.php
+++ spanTitle/spanTitle.php
@@ -7,12 +7,21 @@
 		parent::__construct($default_lang);
 		
 		# déclaration des hooks
+		$this->addHook('IndexBegin', 'IndexBegin');
 		$this->addHook('spanMainLinkTitle', 'spanMainLinkTitle');
 		$this->addHook('spanMainTitle','spanMainTitle');
         
         # droits pour accèder à la page config.php du plugin
 		$this->setConfigProfil(PROFIL_ADMIN);
     }
+        // désactive de force la compression gzip si activée pour une compatibilité des plugins usant du hook indexEnd() ou hook perso similaire dans les templates
+        public function  IndexBegin() {
+            echo '<?php ';
+			?>
+        $plxMotor->aConf['gzip'] ='0';
+            ?>
+			<?php           
+        }
 		
 		#desc hook function
     public function spanMainLinkTitle()
